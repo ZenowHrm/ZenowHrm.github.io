9859cd89ef0d5ba98998be6f458cc01a436b67d3
ZenowHrm <152890552+ZenowHrm@users.noreply.github.com>
2024-05-10 19:36:51 -0400

    actualizacion

---
+++ app.js
@@ -0,0 +1,66 @@
+const COUNT = 50;
+
+const SIZES = [
+    'rainDrop--s',
+    'rainDrop--s',
+    'rainDrop--s',
+    'rainDrop--s',
+    'rainDrop--m',
+    'rainDrop--m',
+    'rainDrop--m',
+    'rainDrop--m',
+    'rainDrop--m',
+    'rainDrop--l',
+    'rainDrop--l',
+    'rainDrop--xl',
+];
+
+const EMOJIS = [
+    '✨',
+    '🩷',
+    '💗',
+    '🌟',
+    '❤️',
+    '💕',
+    '🎀',
+    '✨',
+    '🌺',
+    '🌸',
+    '⭐',
+    '🌟',
+    '💌',
+    '🎈',
+    '🎉'
+];
+
+const rainContainer = document.querySelector('.rain-container');
+
+const genRainDrop = (size, xStart, xEnd, yStart, emoji)=>{
+    const myEmoji = document.createElement('div');
+    myEmoji.innerText = emoji
+    myEmoji.classList.add('rainDrop', size);
+    myEmoji.style.setProperty('--x-start', xStart + 'vw');
+    myEmoji.style.setProperty('--x-end', xEnd + 'vw');
+    myEmoji.style.setProperty('--y-start', yStart + 'vh');
+    myEmoji.style.setProperty('--y-end', yStart + 200 +'vh');
+
+    return myEmoji
+}
+
+for(let i=0; i<COUNT; i++){
+    const size = randFromList(SIZES);
+    const xStart = getRamdom(0,100);
+    const xEnd = getRamdom(xStart - 20, xStart + 20);
+    const yStart = getRamdom(-100,0);
+    const emoji = randFromList(EMOJIS);
+
+    rainContainer.appendChild(genRainDrop(size, xStart, xEnd, yStart, emoji));
+}
+
+function randFromList(items){
+    return items[Math.floor(Math.random()*items.length)];
+}
+
+function getRamdom(min, max){
+    return Math.random()*(max - min) + min;
+}
